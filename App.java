d90738855b426d2417de31102bb72c846541dff3
bascode-dev <bksamson30@gmail.com>
2024-04-25 11:08:10 +0100

    Added addittion method

--- App.java
+++ App.java
@@ -1,9 +1,16 @@
 
 public class App{
+
+public static int add(int add1, int add2 ){
+ return add1 + add2;
+}	
+	
 public static void main(String... args){
 
 System.out.println("Hello it working");
 System.out.println("change made");
+   
+ System.out.println(add(20, 40));
 
 }
 
