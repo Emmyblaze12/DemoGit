adceaa7fde3d1a7b3c2c86ac4dd2364b13634e35
Emmyblaze12 <ajagbaemmanuel@gmail.com>
2024-04-25 13:12:21 +0100

    Added addition function to the python file

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ def multiply(num1,num2):
     result2 = num1*num2
     print(result2)
 
+def add(num1,num2):
+    result3 = num1+num2
+    print(result3)
 
+addition(4,5)
 multiply(6,2)
 divide(6,2)
 
